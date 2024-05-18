f06236a5331e35c89beec4d58893837a9bf6a65d
Ivan-dude <ivandude87@mail.ru>
2024-05-18 18:02:43 +1000

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+name = ('Ivan') #тип данных str
+print(name)
+age = (36) #тип данных int
+print(age)
+age = (age+1) #тип данных int
+print(age)
+is_student = (True) #тип данных bool
+print(is_student)
